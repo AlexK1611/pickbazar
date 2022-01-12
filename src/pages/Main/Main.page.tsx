b58c6e8d0329b08b18f75f83ccdb4d4662de5d5c
AlexK1611 <iltr1611@gmail.com>
2022-01-12 14:53:53 +0200

    cart and cart button for main and product pages

--- src/pages/Main/Main.page.tsx
+++ src/pages/Main/Main.page.tsx
@@ -2,8 +2,8 @@ import { FC, useState } from 'react'
 import { TopSection } from './components/TopSection/TopSection.component'
 import { Coupons } from './components/Coupons/Coupons.component'
 import { Products } from './components/Products/Products.component'
-import { CartButton } from './components/CartButton/CartButton.component'
-import { Cart } from './components/Cart/Cart.component'
+import { CartButton } from 'components/CartButton/CartButton.component'
+import { Cart } from 'components/Cart/Cart.component'
 
 export const Main: FC = () => {
     const [isCart, handleCart] = useState(false)
