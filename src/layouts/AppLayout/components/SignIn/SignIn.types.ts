e87222e59c6808766b2b25a03fd666ea86b5bf37
AlexK1611 <iltr1611@gmail.com>
2022-02-03 14:12:43 +0200

    func args names update

--- src/layouts/AppLayout/components/SignIn/SignIn.types.ts
+++ src/layouts/AppLayout/components/SignIn/SignIn.types.ts
@@ -1,5 +1,5 @@
 import { AuthFormTypes } from '../AuthModal/AuthModal.types'
 
 export interface SignInProps {
-    formTypeHandler: (arg: AuthFormTypes) => () => void
+    formTypeHandler: (formType: AuthFormTypes) => () => void
 }
